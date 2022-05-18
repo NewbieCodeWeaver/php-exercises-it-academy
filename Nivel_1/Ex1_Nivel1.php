eb63ee50ffc0ecf2d8cdc400b0c856cc01c3bc85
Newbiecodeweaver <carlosgilino@gmail.com>
2022-05-18 13:33:09 +0200

    primera entrega php basico

---
+++ Nivel_1/Ex1_Nivel1.php
@@ -0,0 +1,15 @@
+<?php
+
+
+// Defineix una variable de cada tipus: integer,double,string i boolean. Després, imprimeix-les per pantalla.
+
+
+$entero = 5;
+$double = 5.5;
+$string = "hola mundo";
+$boolean = True;
+
+
+echo "Variable de tipo entero: " .$entero. "<br>". "Variable de tipo double: " .$double. "<br>". "Variable de tipo String: ".$string. "<br>". "Variable de tipo boolean: " .$boolean;
+
+?>
