46bf9bca09463d4592e2d08b3f0cd2fc44c8338d
Newbiecodeweaver <carlosgilino@gmail.com>
2022-05-18 13:36:19 +0200

    cuarta entrega php basico

---
+++ Nivel_1/Ex4_Nivel1.php
@@ -0,0 +1,31 @@
+<?php
+
+
+$x = 5;
+$y = 10;
+
+$n = 5.5;
+$m = 10.5;
+
+echo "El valor de x es " . $x . ". El valor de y es " . $y . "<br>";
+echo "La suma de x e y es " . ($x + $y) . "<br>";
+echo "La resta de x e y es " . ($x - $y) . "<br>";
+echo "El producto de x e y es " . ($x * $y) . "<br>";
+echo "El modulo de x e y es " . ($x % $y) . "<br>";
+
+echo "El valor de n es " . $n . ". El valor de m es " . $m . "<br>";
+echo "La suma de n y m es " . ($n + $m) . "<br>";
+echo "La resta de n y m es " . ($n - $m) . "<br>";
+echo "El producto de m y n es " . ($n - $m) . "<br>";
+echo "El modulo de m y n es " . ($n % $m) . "<br>";
+
+echo "El doble de x es " . ($x * 2) . "<br>";
+echo "El doble de y es " . ($y * 2) . "<br>";
+echo "El doble de m es " . ($m * 2) . "<br>";
+echo "El doble de n es " . ($n * 2) . "<br>";
+
+echo "La suma de todas las variables es " . ($x + $y + $m + $n) . "<br>";
+
+echo "El producto de todas las variables es " . ($x + $y + $m + $n) . "<br>";
+
+?>
